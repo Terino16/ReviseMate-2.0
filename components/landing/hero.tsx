a1bb80b6947313c67c25840facb20bf11f406376
Anubhav Dube <123401184+Terino16@users.noreply.github.com>
2026-04-01 22:19:27 +0530

    adding base files

--- components/landing/hero.tsx
+++ components/landing/hero.tsx
@@ -16,7 +16,7 @@ export default function Hero() {
             <div className="absolute inset-0 bg-white/40" />
             <div className="relative max-w-[1100px] mx-auto px-4 sm:px-6 py-10 sm:py-20 h-screen">
                 <Navbar />
-                <div className="flex flex-col items-center justify-center space-y-6 sm:space-y-8 mt-[100px] sm:mt-[80px]">
+                <div className="flex flex-col items-center justify-center space-y-6 sm:space-y-8 mt-[150px] sm:mt-[100px]">
                     <h1 className="hero-in-1 text-[3rem] sm:text-[3rem] md:text-[4rem] lg:text-[4.5rem] font-serif text-center font-light leading-[120%] sm:leading-[115%] px-2">
                         ReviseMate is a learning <br className="hidden sm:block" /> platform designed for:
                     </h1>
@@ -35,13 +35,20 @@ export default function Hero() {
                 </div>
 
                 {/* Dashboard image flush to bottom, slightly elevated */}
-                <div className="hero-in-3 absolute bottom-[-60px] mt-16 sm:mt-16  w-full px-2 flex items-end">
+                <div className="hero-in-3 absolute left-4 right-4 sm:left-6 sm:right-6 bottom-4 sm:bottom-[-60px] flex items-end justify-center">
                     <Image
                         src="/Dashboard_Image.png"
                         alt="Hero Image"
                         width={1000}
                         height={1400}
-                        className="w-full h-auto object-cover object-top rounded-t-2xl shadow-2xl"
+                        className="w-full h-auto object-cover object-top rounded-t-2xl shadow-2xl hidden sm:block"
+                    />
+                    <Image
+                        src="/Dashboard.png"
+                        alt="Hero Image"
+                        width={1000}
+                        height={1400}
+                        className="w-full h-auto object-cover object-top rounded-t-2xl shadow-2xl block sm:hidden"
                     />
                 </div>
             </div>
