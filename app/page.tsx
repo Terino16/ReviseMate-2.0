4ffe2a821686d400b13dbb1dd18fd1e76995c12f
Anubhav Dube <123401184+Terino16@users.noreply.github.com>
2026-04-02 09:34:45 +0530

    adding base files

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,20 @@
-// import Hero from "../components/landing/hero"
-// import Feature from "../components/landing/feature"
-// import HowItWorks from "../components/landing/howitworks"
-// import Testimonials from "../components/landing/testimonials"
-// import Pricing from "../components/landing/pricing"
-// import Footer from "../components/landing/footer"
-import Waiting from "../components/landing/waiting"
+import Hero from "../components/landing/hero"
+import Feature from "../components/landing/feature"
+import HowItWorks from "../components/landing/howitworks"
+import Testimonials from "../components/landing/testimonials"
+import Pricing from "../components/landing/pricing"
+import Footer from "../components/landing/footer"
+
 
 export default function Home() {
   return (
     <div className="bg-[#F2EDEC]">
-      <Waiting />
-     {/* <Hero/>
+     <Hero/>
       <Feature/>
       <HowItWorks/>
       <Testimonials/>
       <Pricing/>
-      <Footer/> */}
+      <Footer/>
     </div>
   )
 }
